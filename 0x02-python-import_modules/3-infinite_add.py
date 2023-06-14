c4561de243d9d936e29ec4263d5079fb01c76bbc
chihab37 <chihab37@gmail.com>
2023-06-14 03:23:41 -0700

    fixed again

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+import sys
+if __name__ == '__main__':
+sys.argv.pop(0)
+	total = 0
+	for number in sys.argv:
+total += int(number)
+	print("{}".format(total))
