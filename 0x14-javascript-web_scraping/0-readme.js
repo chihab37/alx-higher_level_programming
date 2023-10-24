b43219b947e4c300a954effc62e62afea733f74d
chihab37 <chihab37@gmail.com>
2023-10-24 08:08:26 -0700

    0x142

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,5 @@
+#!/usr/bin/node
 const fs = require('fs');
-
-// Check if a file path argument was provided
-if (process.argv.length < 3) {
-    console.log('Please provide a file path as an argument.');
-    process.exit(1);
-}
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data.trim());  // Using trim() to remove any trailing newline
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
-
